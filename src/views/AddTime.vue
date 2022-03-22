62b473962e05f8abde02026cce0cfbbe37086c70
sachin-ezee <sachin.devaiya@ezeetechnosys.com>
2022-03-22 15:11:41 +0530

    Add Time component Update

--- src/views/AddTime.vue
+++ src/views/AddTime.vue
@@ -8,13 +8,13 @@
           label-for="input-0">
           <b-form-datepicker
             id="input-0"
-            v-model="user.dateSelect"
+            v-model="time.dateSelect"
             required
             now-button
-            :disabled="user.isEdit"
+            :disabled="time.isEdit"
             placeholder="Select Date">
           </b-form-datepicker>
-          <span v-if="submitted && $v.user.dateSelect.$error" class="errorMsg">Please Select Date</span>
+          <span v-if="submitted && $v.time.dateSelect.$error" class="errorMsg">Please Select Date</span>
         </b-form-group>
         <b-form-group
           id="input-group-1"
@@ -22,13 +22,13 @@
           label-for="input-1">
           <b-form-timepicker
             id="input-1"
-            v-model="user.startTime"
+            v-model="time.startTime"
             required
             now-button
-            :disabled="user.isEdit"
+            :disabled="time.isEdit"
             placeholder="Select Start Time">
           </b-form-timepicker>
-          <span v-if="submitted && $v.user.startTime.$error" class="errorMsg">Please Select Start Time</span>
+          <span v-if="submitted && $v.time.startTime.$error" class="errorMsg">Please Select Start Time</span>
         </b-form-group>
         <b-form-group
           id="input-group-2"
@@ -36,13 +36,13 @@
           label-for="input-2">
           <b-form-timepicker
             id="input-2"
-            v-model="user.endTime"
+            v-model="time.endTime"
             required
               now-button
-            :disabled="user.isEdit"
+            :disabled="time.isEdit"
             placeholder="Select End Time">
           </b-form-timepicker>
-          <span v-if="submitted && $v.user.endTime.$error" class="errorMsg">Please Enter Employee Name</span>
+          <span v-if="submitted && $v.time.endTime.$error" class="errorMsg">Please Select End Time</span>
         </b-form-group>
         <b-form-group
           id="input-group-3"
@@ -50,14 +50,14 @@
           label-for="input-3">
           <b-form-textarea
             id="input-3"
-            v-model="user.taskDesc"
+            v-model="time.taskDesc"
             required
             rows="3"
             max-rows="6"
             placeholder="Enter Task Description">
           </b-form-textarea>
-          <div v-if="submitted && $v.user.taskDesc.$error" class="errorMsg">
-            Please Enter Email address
+          <div v-if="submitted && $v.time.taskDesc.$error" class="errorMsg">
+            Please Enter Task Description
           </div>
         </b-form-group>
       </b-form>
@@ -85,38 +85,38 @@
 
 <script>
 import { BModal, BForm } from 'bootstrap-vue'
-import { required, minLength, maxLength, email } from 'vuelidate/lib/validators'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
-  name: 'add_new_employee',
+  name: 'add_new_time',
   data () {
     return {
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        contact: ''
+      time: {
+        dateSelect: '',
+        startTime: '',
+        endTime: '',
+        taskDesc: ''
       },
       submitted: false
     }
   },
   validations: {
-    user: {
-      id: { required },
-      name: { required },
-      email: { required, email },
-      contact: { required, minLength: minLength(10), maxLength: maxLength(12) }
+    time: {
+      dateSelect: { required },
+      startTime: { required },
+      endTime: { required },
+      taskDesc: { required }
     }
   },
   mounted () {
-    this.$root.$on('edit-employee', (data) => {
-      this.user = data
-      this.user.isEdit = true
+    this.$root.$on('edit-time', (data) => {
+      this.time = data
+      this.time.isEdit = true
       this.submitted = false
     })
-    this.$root.$on('add-employee', (data) => {
-      this.user = {}
-      this.user.isEdit = false
+    this.$root.$on('add-time', (data) => {
+      this.time = {}
+      this.time.isEdit = false
       this.submitted = false
     })
   },
@@ -131,12 +131,12 @@ export default {
       if (this.$v.$invalid) {
         return
       }
-      this.$bvModal.hide('addNewEmployee')
-      this.$store.dispatch('updatelistEmployee', { employee: this.user }) // dispatch store action
+      this.$bvModal.hide('addNewTime')
+      this.$store.dispatch('updatelistTime', { times: this.time }) // dispatch store action
     },
     resetForm () {
-      this.user = {}
-      this.user.isEdit = false
+      this.time = {}
+      this.time.isEdit = false
       this.submitted = false
     }
   }
